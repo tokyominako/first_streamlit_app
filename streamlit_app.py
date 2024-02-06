113600a452c69e18cac8af0a08ba18b7616eac29
Karen <78629829+tokyominako@users.noreply.github.com>
2024-02-06 10:42:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 
 streamlit.title('My parent new healthy dinner')
-streamlit.title('egg bites muffin')
+streamlit.header('Breafast menu')
+streamlit.text('omega 3 & blueberry oatmeal')
+streamlit.text('kale smoothie')
+streamlit.text('free range hard boiled egg')
